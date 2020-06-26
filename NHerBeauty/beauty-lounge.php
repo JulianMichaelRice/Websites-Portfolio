3815bf3893955621a836c41026e5c6173f28ab4a
Julian M. Rice <rice.m.julian@gmail.com>
2020-06-26 01:54:13 -0700

    NHerBeauty: Stripe Payment Integrated with PHP, 90% Ready Website

--- NHerBeauty/beauty-lounge.php
+++ NHerBeauty/beauty-lounge.php
@@ -32,7 +32,7 @@
                 <a class="nav-link" href="blog">Blog</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="beauty-lounge" >The Beauty Lounge</a>
+              <a class="nav-link" href="">The Beauty Lounge</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="shop">Shop</a>
@@ -56,43 +56,82 @@
     <!-- Home -->
     <div class="spacer-s"></div>
 
-    <!-- Blog -->
-    <div class="container-fluid">
-      <h1 class="text-center hidden-o">Blog</h1>
-      <div class="spacer-m"></div>
-      <div class="row">
-        <div class="col-md-4 col-12 hidden-l hoverer" onmouseover="priority(0,'on');" onmouseout="priority(0,'off')" style="padding: 0;">
-          <div class="blog blog-wallpaper-1 cover-mid" style="padding: 30px;">
-            <h5 class="text-center">Blog Post 1</h5>
-            <div class="text-center">
-              <a href="" class="btn quickhalf display-center" target="_blank">Read More</a>
-            </div>
+    <?php if (!isset($_SESSION['Username'])) { ?>
+    <!-- The Beauty Lounge -->
+    <div class="spacer-s"></div>
+    <div class="parallax cover-tall carousel-bg4 hidden-o"></div>
+    <div class="spacer-m"></div>
+    <div class="panel text-center hidden-o2">
+      <form action="payment" method="POST" id="paymentFrm">
+          <div class="panel-heading">
+              <h3 class="panel-title">Join the Beauty Lounge</h3>
+              <div class="spacer-m"></div>
+              <!-- Plan Info -->
+              <p>
+                  <b>Plan </b>
+                  <select name="subscr_plan" id="subscr_plan">
+                      <?php foreach($plans as $id=>$plan){ ?>
+                          <option value="<?php echo $id; ?>"><?php echo $plan['name'].' [$'.$plan['price'].'/'.$plan['interval'].']'; ?></option>
+                      <?php } ?>
+                  </select>
+              </p>
           </div>
-        </div>
-        <div class="col-md-4 col-12 hidden hoverer" onmouseover="priority(1,'on');" onmouseout="priority(1,'off')" style="padding: 0;">
-          <div class="blog blog-wallpaper-2 cover-mid" style="padding: 30px;">
-            <h5 class="text-center">Blog Post 2</h5>
-            <div class="text-center">
-              <a href="" class="btn quickhalf display-center" target="_blank">Read More</a>
-            </div>
+          <div class="panel-body">
+              <!-- Display errors returned by createToken -->
+              <div class="card-errors"></div>
+        
+              <!-- Payment form -->
+              <div class="row">
+                <div class="form-group col-12 col-md-4">
+                  <label>Full Name</label>
+                  <input type="text" name="name" id="name" placeholder="Enter name" required="" autofocus="">
+                </div>
+                <div class="form-group col-12 col-md-4">
+                  <label>Email</label>
+                  <input type="email" name="email" id="email" placeholder="Enter email" required="">
+                </div>
+                <div class="form-group col-12 col-md-4">
+                  <label>Password</label>
+                  <input type="password" name="password" id="password" autocomplete="off" required="">
+                </div>
+              </div>
+              
+              <div class="row">
+                <div class="col-0 col-md-2"></div>
+                <div class="form-group col-12 col-md-8">
+                  <label>Card Number</label>
+                  <input type="text" name="card_number" id="card_number" placeholder="1234 1234 1234 1234" maxlength="16" autocomplete="off" required="">
+                </div>
+                <div class="col-0 col-md-2"></div>
+              </div>
+              <div class="row">
+                <div class="form-group col-12 col-md-7">
+                  <label>Expiry Date</label>
+                  <input type="text" name="card_exp_month" id="card_exp_month" placeholder="MM" maxlength="2" required="">
+                  <input type="text" name="card_exp_year" id="card_exp_year" placeholder="YYYY" maxlength="4" required="">
+                </div>
+                <div class="form-group col-12 col-md-5">
+                  <label>CVC</label>
+                  <input type="text" name="card_cvc" id="card_cvc" placeholder="CVC" maxlength="3" autocomplete="off" required="">
+                </div>
+              </div>
+              <button type="submit" class="btn btn-gold" id="payBtn">Join the Beauty Lounge</button>
           </div>
-        </div>
-        <div class="col-md-4 col-12 hidden-r hoverer" onmouseover="priority(2,'on');" onmouseout="priority(2,'off')" style="padding: 0;">
-          <div class="blog blog-wallpaper-3 cover-mid" style="padding: 30px;">
-            <h5 class="text-center">Blog Post 3</h5>
-            <a href="" class="btn quickhalf display-center" target="_blank">Read More</a>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4 col-6 hidden-l hoverer" onmouseover="priority(3,'on');" onmouseout="priority(3,'off')" style="padding: 0;">
-          <div class="blog blog-wallpaper-4 cover-mid" style="padding: 30px;">
-            <h5 class="text-center">Blog Post 4</h5>
-            <a href="" class="btn quickhalf display-center" target="_blank">Read More</a>
-          </div>
-        </div>
-      </div>
+      </form>
     </div>
+    <?php } ?>
+
+    <?php if (isset($_SESSION['Username'])) { ?>
+    <!-- The Beauty Lounge Logged In -->
+    <div class="container-fluid">
+        <div class="spacer-s"></div>
+        <div class="parallax cover-tall carousel-bg4 hidden-o"></div>
+        <div class="spacer-s"></div>
+        <h1 class="text-center hidden-o">The Beauty Lounge</h1>
+        <div class="spacer-s"></div>
+    </div>
+        
+    <?php } ?>
 
     <?php if (isset($_SESSION['errors'])): ?>
         <?php foreach ($_SESSION['errors'] as $error): ?>
@@ -131,6 +170,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://js.stripe.com/v2/"></script>
     <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
     <script src="animatedModal.min.js"></script>
     <script src="javascript.js" type="text/javascript"></script>
@@ -158,5 +198,46 @@
         }
       }
     </script>
+    <script>
+      // Set your publishable key
+      Stripe.setPublishableKey('<?php echo STRIPE_PUBLISHABLE_KEY; ?>');
+      
+      // Callback to handle the response from stripe
+      function stripeResponseHandler(status, response) {
+          if (response.error) {
+              // Enable the submit button
+              $('#payBtn').removeAttr("disabled");
+              // Display the errors on the form
+              $(".payment-status").html('<p>'+response.error.message+'</p>');
+          } else {
+              var form$ = $("#paymentFrm");
+              // Get token id
+              var token = response.id;
+              // Insert the token into the form
+              form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
+              // Submit form to the server
+              form$.get(0).submit();
+          }
+      }
+      
+      $(document).ready(function() {
+          // On form submit
+          $("#paymentFrm").submit(function() {
+              // Disable the submit button to prevent repeated clicks
+              $('#payBtn').attr("disabled", "disabled");
+          
+              // Create single-use token to charge the user
+              Stripe.createToken({
+                  number: $('#card_number').val(),
+                  exp_month: $('#card_exp_month').val(),
+                  exp_year: $('#card_exp_year').val(),
+                  cvc: $('#card_cvc').val()
+              }, stripeResponseHandler);
+          
+              // Submit from callback
+              return false;
+          });
+      });
+      </script>
   </body>
 </html>
--- NHerBeauty/pdoconfig.php
+++ NHerBeauty/pdoconfig.php
@@ -1,12 +1,19 @@
-<!-- <?php
-    $host = 'localhost';
-    $dbname = 'u924172280_sprouttutor';
-    $username = 'u924172280_julianrice';
-    $password = 'yasirTUTOR1';
-?> -->
 <?php
+	$plans = array( 
+    '1' => array( 
+        'name' => 'The Beauty Lounge', 
+        'price' => 14.99, 
+        'interval' => 'month' 
+    )
+); 
+$currency = "usd";  
+ 
+// Stripe API configuration  
+define('STRIPE_API_KEY', 'sk_test_51GxCF8AJIvuLye2bv3xZWWKZMXkg7maugs2afcolW9qbattq2Z6o1cPswqiAUigeP0c6zraWeKB3XQyJ8NN0yQOY00VxA05dzW');
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51GxCF8AJIvuLye2bhVlNaDesbRKBN4HFndCuywAgEKATxz8GmyK95DsfNhWFJt5vEsbioAUO68vcAtvFDngv1Ray00K9EiFNar'); 
+
     $host = 'localhost';
-    $dbname = 'logintest';
+    $dbname = 'nherbeauty';
     $username = 'root';
     $password = '';
 ?>
